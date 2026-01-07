b0dd4df60bd9621e2dc4f98cd1444ca07a73370e
Hannah Lynn Lindrob <hannahlindrob@outlook.com>
2026-01-07 16:39:41 +0100

    Implemented user auth and guards

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, ValidationPipe, Post } from '@nestjs/common';
+import { Body, Controller, ValidationPipe, Post, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services';
 import { Account } from '@persistance/entities';
 import { SignInDto } from '../dtos';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly service: AuthService) { }
+
+
     @Post('login')
     async login(@Body(new ValidationPipe()) credentials: SignInDto) {
         return await this.service.login(credentials);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
     }
 }
